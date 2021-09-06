a2fb11ea6de78ffb485038fe3db8c4c1649a29a4
Jéssica Nathany <jessicanathany.web@gmail.com>
2021-09-06 18:51:44 -0300

    debugando código do hacker rank

--- estrutura-dados-csharp/estrutura-dados-csharp/3- Desafios HackerRank/VeryBigSum.cs
+++ estrutura-dados-csharp/estrutura-dados-csharp/3- Desafios HackerRank/VeryBigSum.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace estrutura_dados_csharp._3__Desafios_HackerRank
 {
     public class VeryBigSum
     {
-        public static long CalculateBigSum(List<long> ar)
+        public static long CalculateBigSum(List<long> arrayNumber)
         {
-            return 0;
+            return  arrayNumber.Sum(x => x);
         }
-
 
         public void SolveSolutionVeryBigSum()
         {
-            //var textWriter = Console.WriteLine("");
+            /*
+             Sample Input:  
+              
+              arrayCount = 5
+              array =  1000000001 1000000002 1000000003 1000000004 1000000005;
 
-            //int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+              Sample Output: 5000000015
+            */
 
-            //List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+            Console.WriteLine("Write the array count: ");
+            Console.WriteLine();
+            int arrayCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-            //long result = VeryBigSum(ar);
+            Console.WriteLine("Write the array numbers: ");
+            Console.WriteLine();
+            var arrayNumber = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrayTemp => Convert.ToInt64(arrayTemp)).ToList();
 
-            //textWriter.WriteLine(result);
-
-            //textWriter.Flush();
-            //textWriter.Close();
+            var result = CalculateBigSum(arrayNumber);
+            Console.WriteLine(result);
         }
     }
 }
